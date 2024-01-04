c4696cbdce5ff400fb2631534adb62c55d94be6f
manjaro23 <152917540+manjaro23@users.noreply.github.com>
2024-01-04 18:51:10 +0300

    Add files via upload

---
+++ install-archbased.sh
@@ -0,0 +1,6 @@
+echo "Welcome! (arch version)"
+sudo pacman -Sy i3 nitrogen gnome-terminal
+echo "installed"
+cp config ~/.config/i3
+echo "Config coppied (probably)"
+
---
+++ install-ubuntu_debian.sh
@@ -0,0 +1,6 @@
+echo "Welcome! (ubuntu version)"
+sudo apt install i3 nitrogen gnome-terminal
+echo "installed"
+cp config ~/.config/i3
+echo "Config coppied (probably)"
+
